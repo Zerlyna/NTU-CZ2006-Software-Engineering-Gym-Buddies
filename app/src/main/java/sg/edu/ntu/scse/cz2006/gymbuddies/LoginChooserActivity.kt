49adce13793ad56afc1f810b0e595bd1050c0f88
itachi1706 <kennethsohyq@gmail.com>
2019-09-03 15:48:03 +0800

    Create a shell activity so that we can actually start like the emulator

---
+++ app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/LoginChooserActivity.kt
@@ -0,0 +1,12 @@
+package sg.edu.ntu.scse.cz2006.gymbuddies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginChooserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_chooser)
+    }
+}
