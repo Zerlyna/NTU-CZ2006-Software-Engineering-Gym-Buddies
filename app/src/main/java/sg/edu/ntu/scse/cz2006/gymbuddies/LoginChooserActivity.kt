0c0b37f5c2c5b0d7e414830b796d8b9aba64a86e
itachi1706 <kennethsohyq@gmail.com>
2019-09-03 16:00:35 +0800

    Added Firebase Implementation

--- app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/LoginChooserActivity.kt
+++ app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/LoginChooserActivity.kt
@@ -2,11 +2,16 @@ package sg.edu.ntu.scse.cz2006.gymbuddies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class LoginChooserActivity : AppCompatActivity() {
+
+    private lateinit var mFirebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_chooser)
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 }
