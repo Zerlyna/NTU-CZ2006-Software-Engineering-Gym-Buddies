a2812b1bc876397e33ade2f99183fd0aef986054
itachi1706 <kennethsohyq@gmail.com>
2019-09-09 19:15:00 +0800

    NTUSE-52 Initialized Google Maps View and setup location permission request
    Resolves NTUSE-52

--- app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/MainActivity.java
+++ app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
@@ -37,12 +38,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show());
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -109,5 +105,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/ui/home/HomeFragment.java
+++ app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/ui/home/HomeFragment.java
@@ -1,35 +1,143 @@
 package sg.edu.ntu.scse.cz2006.gymbuddies.ui.home;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
 
 import sg.edu.ntu.scse.cz2006.gymbuddies.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     private HomeViewModel homeViewModel;
+    private MapView mapView;
+    private GoogleMap mMap;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    private static final int RC_LOC = 1001, RC_LOC_BTN = 1002;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        homeViewModel.getText().observe(this, s -> textView.setText(s));
+
+        mapView = root.findViewById(R.id.map_view);
+
+        // Request permission eh
+        if (!hasGpsPermission() && getActivity() != null) {
+            Log.i(TAG, "No permissions, requesting...");
+            final String[] permissions = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(getActivity()).setTitle("Location Permission Required").setMessage("We require access to your location to view nearby gyms")
+                        .setPositiveButton(android.R.string.ok, (dialog, which) -> requestPermissions(permissions, RC_LOC)).show();
+            } else {
+                requestPermissions(permissions, RC_LOC);
             }
-        });
+        }
+
         return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "Google Map Ready");
+        mMap = googleMap;
+        mMap.setTrafficEnabled(true);
+        hasGps(true);
+        //checkGpsForCurrentLocation();
+        UiSettings settings = mMap.getUiSettings();
+        //settings.setZoomControlsEnabled(true);
+        settings.setMapToolbarEnabled(false);
+
+        //zoomToLocation();
+    }
+
+    private boolean hasGpsPermission() {
+        return hasGps(false);
+    }
+
+    private boolean hasGps(boolean startCheck) {
+        if (getContext() == null) return false;
+        boolean permGranted = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        if (permGranted && startCheck) {
+            mMap.setMyLocationEnabled(true);
+        }
+        return permGranted;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != RC_LOC && requestCode != RC_LOC_BTN) {
+            Log.d(TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Location permission granted - initialize the gps source");
+            // we have permission, check if map is enabled
+            if (mMap != null) {
+                hasGps(true);
+            }
+
+            if (requestCode == RC_LOC_BTN) {
+                // TODO: When FAB clicked do something
+            }
+            return;
+        }
+
+        Log.e(TAG, "Permission not granted: results len = " + grantResults.length + " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
+
+        if (requestCode == RC_LOC_BTN && getActivity() != null) {
+            new AlertDialog.Builder(getActivity()).setTitle("Location Permission not granted")
+                    .setMessage("Unable to get location, permission not granted").setPositiveButton(android.R.string.ok, null)
+                    .setNeutralButton("App Settings", (dialog, which) -> {
+                        Intent permIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri packageURI = Uri.parse("package:" + getActivity().getPackageName());
+                        permIntent.setData(packageURI);
+                        startActivity(permIntent);
+                    }).show();
+        }
+    }
+
+    private static final String TAG = "HomeFrag";
 }
