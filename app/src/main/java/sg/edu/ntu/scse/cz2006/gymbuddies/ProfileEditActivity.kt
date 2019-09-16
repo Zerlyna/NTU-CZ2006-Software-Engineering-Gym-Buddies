dc7852696060cf872499489cc48257976e187a7f
itachi1706 <kennethsohyq@gmail.com>
2019-09-16 18:43:16 +0800

    NTUSE-60 Implemented back logging out when you have not completed profile creation
    This overrides someone attempt to bypass the profile creation to be honest hahaha

--- app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/ProfileEditActivity.kt
+++ app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/ProfileEditActivity.kt
@@ -1,12 +1,35 @@
 package sg.edu.ntu.scse.cz2006.gymbuddies
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileEditActivity : AppCompatActivity() {
 
+    private var firstRun = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_edit)
+
+        firstRun = intent.getBooleanExtra("firstrun", false)
+        Log.d(TAG, "FirstRun: $firstRun")
     }
+
+    override fun onBackPressed() {
+        if (firstRun) {
+            // Log user out
+            finish()
+            val logout = Intent(this, LoginChooserActivity::class.java).apply { putExtra("logout", true) }
+            startActivity(logout)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    companion object {
+        private const val TAG = "ProfileEdit"
+    }
+
 }
