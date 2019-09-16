e03c4867d430c03caaaa8cb6cc526dbc4ca9a89d
itachi1706 <kennethsohyq@gmail.com>
2019-09-16 23:58:39 +0800

    NTUSE-60 Added ability to upload profile pictures when editing profiles
    Clicking on the profile picture allows the user to select their own profile picture from their device (no camera for now)
    After selection it will automatically compress said image into jpeg and upload to cloud storage, retrieving download url
    This download url will be saved into the database for reference, it will also be placed into the auth profile as the profile pic
    
    We renamed retrieving profile pic to retrieve from FBAuth as it is no longer just from Google accounts

--- app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/ProfileEditActivity.kt
+++ app/src/main/java/sg/edu/ntu/scse/cz2006/gymbuddies/ProfileEditActivity.kt
@@ -1,11 +1,14 @@
 package sg.edu.ntu.scse.cz2006.gymbuddies
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
@@ -13,18 +16,23 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_profile_edit.*
 import kotlinx.android.synthetic.main.row_pref_days.*
 import sg.edu.ntu.scse.cz2006.gymbuddies.datastruct.User
-import sg.edu.ntu.scse.cz2006.gymbuddies.tasks.GetProfilePicFromGoogle
+import sg.edu.ntu.scse.cz2006.gymbuddies.tasks.GetProfilePicFromFirebaseAuth
 import sg.edu.ntu.scse.cz2006.gymbuddies.tasks.UpdateFirebaseFirestoreDocument
+import sg.edu.ntu.scse.cz2006.gymbuddies.tasks.UploadProfilePic
 import sg.edu.ntu.scse.cz2006.gymbuddies.util.InputHelper
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ProfileEditActivity : AppCompatActivity() {
 
     private var firstRun = false
     private var profileImage: Bitmap? = null
     private var profileUri: Uri? = null
+    private lateinit var uid: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,16 +41,19 @@ class ProfileEditActivity : AppCompatActivity() {
         firstRun = intent.getBooleanExtra("firstrun", false)
         Log.d(TAG, "FirstRun: $firstRun")
 
+        // Assume auth always succeeed
+        val auth = FirebaseAuth.getInstance().currentUser!!
+        uid = auth.uid
+
         if (firstRun) {
             supportActionBar?.title = "Create New Profile"
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
             Log.i(TAG, "Create new profile flow called")
-            val auth = FirebaseAuth.getInstance().currentUser!!
             if (!auth.displayName.isNullOrEmpty()) etName.setText(auth.displayName)
-            if (auth.photoUrl != null) {
+            if (auth.photoUrl != null && auth.photoUrl.toString().toLowerCase(Locale.getDefault()) != "null") {
                 // Set Uri of user
                 profileUri = auth.photoUrl
-                GetProfilePicFromGoogle(this, object: GetProfilePicFromGoogle.Callback {
+                GetProfilePicFromFirebaseAuth(this, object: GetProfilePicFromFirebaseAuth.Callback {
                     override fun onComplete(bitmap: Bitmap?) {
                         // Update image drawable
                         profileImage = bitmap
@@ -51,22 +62,25 @@ class ProfileEditActivity : AppCompatActivity() {
                         profile_pic.setImageDrawable(roundBitmap)
                     }
                 }).execute(profileUri)
+            } else {
+                Log.i(TAG, "No image found, showing default image")
+                profile_pic.setImageResource(R.mipmap.ic_launcher) // Default Image
             }
         } else {
             // TODO: Retrieve existing data if not first run from database
         }
         fab.setOnClickListener {
             InputHelper.hideSoftKeyboard(this)
-            if (validate()) {
-                addOrUpdate()
-            } else {
-                Snackbar.make(coordinator, "Please complete your profile before continuing", Snackbar.LENGTH_LONG).show()
+            // Check if uploading image
+            if (loadImage.visibility == View.VISIBLE) Snackbar.make(coordinator, "Currently uploading profile picture. Wait for it to finish before saving profile", Snackbar.LENGTH_LONG).show()
+            else {
+                if (validate()) addOrUpdate()
+                else Snackbar.make(coordinator, "Please complete your profile before continuing", Snackbar.LENGTH_LONG).show()
             }
         }
         profile_pic.setOnClickListener {
-            // TODO: Allow user to take picture or upload picture
-            Snackbar.make(coordinator, "Coming Soon", Snackbar.LENGTH_LONG).show()
-            // TODO: Add profile pic into cloud storage for retrieval
+            val photoIntent = Intent(Intent.ACTION_PICK).apply { type = "image/*" }
+            startActivityForResult(photoIntent, REQUEST_PROFILE_PIC)
         }
     }
 
@@ -92,7 +106,7 @@ class ProfileEditActivity : AppCompatActivity() {
     }
 
     private fun addOrUpdate() {
-        Snackbar.make(coordinator, "Adding uh. IE to say its coming soon", Snackbar.LENGTH_LONG).show()
+        Log.i(TAG, "Doing add/update of profile")
         val name = etName.text.toString()
         val prefLocation = location.selectedItem.toString()
         val gender = findViewById<RadioButton>(radio_gender.checkedRadioButtonId).text.toString()
@@ -102,7 +116,7 @@ class ProfileEditActivity : AppCompatActivity() {
             val user = User(name=name, prefLocation = prefLocation, gender = gender, prefTime = timeRange, prefDay = User.PrefDays(selectedDays), profilePicUri = profileUri.toString())
             user.flags.firstRun = false
             val db = FirebaseFirestore.getInstance()
-            val ref = db.collection("users").document(FirebaseAuth.getInstance().currentUser!!.uid)
+            val ref = db.collection("users").document(uid)
             UpdateFirebaseFirestoreDocument(ref, user, object: UpdateFirebaseFirestoreDocument.Callback {
                 override fun onComplete(success: Boolean) {
                     Log.i(TAG, "Insertion Status: $success")
@@ -111,9 +125,14 @@ class ProfileEditActivity : AppCompatActivity() {
                             setDisplayName(name)
                             if (user.profilePicUri.isNotEmpty()) setPhotoUri(Uri.parse(user.profilePicUri))
                         }
-                        FirebaseAuth.getInstance().currentUser!!.updateProfile(userProfileBuilder.build())
-                        startActivity(Intent(this@ProfileEditActivity, MainActivity::class.java))
-                        finish()
+                        val currentUser = FirebaseAuth.getInstance().currentUser!!
+                        Snackbar.make(coordinator, "Creating Profile...", Snackbar.LENGTH_LONG).show()
+                        currentUser.updateProfile(userProfileBuilder.build()).addOnCompleteListener {
+                            currentUser.reload().addOnCompleteListener {
+                                startActivity(Intent(this@ProfileEditActivity, MainActivity::class.java))
+                                finish()
+                            }
+                        }
                     } else {
                         Snackbar.make(coordinator, "An error occurred updating profile", Snackbar.LENGTH_LONG).show()
                     }
@@ -138,6 +157,31 @@ class ProfileEditActivity : AppCompatActivity() {
         return list
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_PROFILE_PIC && resultCode == Activity.RESULT_OK && data != null) {
+            val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(data.data as Uri))
+            // Upload image into cloud before setting
+            loadImage.visibility = View.VISIBLE
+            val storage = FirebaseStorage.getInstance()
+            val ref = storage.reference.child("profile/${uid}.jpg")
+            UploadProfilePic(this, ref, bitmap, object: UploadProfilePic.Callback {
+                override fun onSuccess(success: Boolean, imageUri: Uri?) {
+                    if (success) {
+                        // Update stuff
+                        profileImage = bitmap
+                        profileUri = imageUri
+                        val roundBitmap = RoundedBitmapDrawableFactory.create(resources, bitmap)
+                        roundBitmap.isCircular = true
+                        profile_pic.setImageDrawable(roundBitmap)
+                    }
+                    loadImage.visibility = View.GONE
+                }
+            }).execute()
+        }
+    }
+
     override fun onBackPressed() {
         if (firstRun) {
             // Log user out
@@ -159,6 +203,7 @@ class ProfileEditActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "ProfileEdit"
+        private const val REQUEST_PROFILE_PIC = 1
     }
 
 }
